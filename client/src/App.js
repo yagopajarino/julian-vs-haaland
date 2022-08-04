ca7b408c50acb62e1caa1c00a17597830517486c
Yago Pajariño <yagopajarino@outlook.com>
2022-08-03 22:31:05 -0300

    Add Header to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer";
+import "./style/index.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className=" bg-gray-50 flex flex-col items-center">
+      <Header />
       <Main />
       <Footer />
     </div>
