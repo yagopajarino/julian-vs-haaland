8c55b92186fdd86458bb53a7e254965011be2810
Yago Pajariño <yagopajarino@outlook.com>
2022-08-03 22:30:39 -0300

    Add Loading component to Main

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,7 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Player from "./components/Player";
+import Loading from "./components/Loading";
+
 export default function Main() {
+  const [golesJuli, setGolesJuli] = useState("");
+  const [golesHaaland, setgolesHaaland] = useState("");
+
+  useEffect(() => {
+    const url = "http://localhost:5000/goles";
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => {
+        setGolesJuli(r.julianGoles);
+        setgolesHaaland(r.erlingGoles);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>This is the main frame</h1>
-    </div>
+    <main
+      className=" w-full flex justify-around items-center"
+      style={{ height: "88vh" }}
+    >
+      {golesJuli == "" ? (
+        <Loading />
+      ) : (
+        <>
+          <Player name="JULIAN" goals={golesJuli} />
+          <Player name="HAALAND" goals={golesHaaland} />
+        </>
+      )}
+    </main>
   );
 }
