17a7aafbe830d6c32e1bdcda2bb1d9e90ac0cabb
Yago Pajariño <yagopajarino@outlook.com>
2022-08-03 22:29:47 -0300

    Change footer structure

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,7 +1,23 @@
+import React from "react";
+import Twitter from "./components/Twitter";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>Yago Pajariño</p>
+    <footer
+      className="w-full p-2 bg-gray-900 text-gray-50 text-center flex space-x-1 items-center justify-center"
+      style={{ height: "6vh" }}
+    >
+      <p className=" text-sm ">
+        <a
+          className="hover:text-slate-300"
+          href="https://github.com/yagopajarino"
+        >
+          Yago Pajariño
+        </a>{" "}
+        |
+      </p>
+      <p className="text-sm">{new Date().getFullYear()} |</p>
+      <Twitter />
     </footer>
   );
 }
