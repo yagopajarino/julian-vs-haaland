e88b3f8d3cd87d694f7add64ee03a775bc1a36c1
Yago Pajariño <yagopajarino@outlook.com>
2023-03-09 21:25:56 -0300

    Change getGoles function

--- server/controller.js
+++ server/controller.js
@@ -1,55 +1,31 @@
-import puppeteer from "puppeteer";
+import axios from "axios";
+
+const getGolesFromHTML = (html) => {
+  const pattern = /(\d+\s+Goles)/;
+  const result = html.match(pattern);
+  let goles = 0;
+  if (result.length > 0) {
+    goles = result[0].split(" ")[0];
+  }
+  return goles;
+};
 
 const controller = {
   sayHello: (req, res, next) => {
     res.send("Hello");
   },
 
-  getGoles: (req, res, next) => {
-    (async () => {
-      const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
-      const page = await browser.newPage();
-      await page.goto(
-        "https://www.transfermarkt.es/manchester-city/leistungsdaten/verein/281"
-      );
-
-      // Get the "viewport" of the page, as reported by the page.
-      const data = await page.evaluate(() => {
-        const rows = new Array(
-          ...document
-            .querySelector("#yw1 > table > tbody")
-            .querySelectorAll("tr")
-        );
-        const julianRow = rows.filter((i) => {
-          const h = i.querySelector(".hauptlink");
-          if (h != null) {
-            return h.querySelector("a").textContent == "Julián Álvarez";
-          } else return false;
-        })[0];
-
-        const erlingRow = rows.filter((i) => {
-          const h = i.querySelector(".hauptlink");
-          if (h != null) {
-            return h.querySelector("a").textContent == "Erling Haaland";
-          } else return false;
-        })[0];
-
-        const julianGoles =
-          julianRow.querySelector("td:nth-child(7)").textContent;
-        const erlingGoles = erlingRow
-          .querySelector("td:nth-child(7)")
-          .textContent.replace("-", "0");
-
-        return {
-          julianGoles,
-          erlingGoles,
-        };
-      });
-
-      res.send(data);
-
-      await browser.close();
-    })();
+  getGoles: async (req, res, next) => {
+    const url_julian =
+      "https://www.fichajes.com/jugador/julian-alvarez/estadistica-de-goles?partial=1&club=manchester-city-fc";
+    const julian = axios.get(url_julian);
+    const url_haland =
+      "https://www.fichajes.com/jugador/erling-haland/estadistica-de-goles?partial=1&club=manchester-city-fc";
+    const haland = axios.get(url_haland);
+    const data = await Promise.all([julian, haland]).then((results) =>
+      results.map((i) => getGolesFromHTML(i.data))
+    );
+    res.send(data);
   },
 };
 
